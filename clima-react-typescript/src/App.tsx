1251f6363e3c8c7fb29e224acfa8ff98046447d8
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-06-24 17:45:43 -0300

    Actualizando proyecto

--- clima-react-typescript/src/App.tsx
+++ clima-react-typescript/src/App.tsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <>
       <h1 className={styles.title}> Buscador de Clima </h1>
+      <div className="{styles.container}">
+        <p>1</p>
+        <p>2</p>
+      </div>
     </>
   )
 }
