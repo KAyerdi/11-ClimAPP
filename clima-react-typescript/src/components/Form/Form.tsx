d5c94166c836f893abe53eda26c6014311f89aff
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-06-26 17:03:06 -0300

    actualizacion proyecto [error en el color del select]

--- clima-react-typescript/src/components/Form/Form.tsx
+++ clima-react-typescript/src/components/Form/Form.tsx
@@ -1,26 +1,54 @@
+import { ChangeEvent, useState } from "react"
+import { countries } from "../../data/countries"
+import type { SearchType } from "../../types"
+import styles from './Form.module.css'
 
 export default function Form() {
+
+  const [search, setSearch] = useState<SearchType>({
+    city:'',
+    country:''
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    setSearch({
+        ...search,
+        [e.target.name] : e.target.value
+    })
+  }
+
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.field}>
         <label htmlFor="city">Ciudad</label>
         <input
           id="city"
           type="text"
           name="city"
           placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange}
         />
       </div>
 
-      <div>
-        <label htmlFor="city">Pais</label>
-        <input
-          id="pais"
-          type="text"
-          name="pais"
-          placeholder="Pais"
-        />
+      <div className={styles.field}>
+        <label htmlFor="country">Pais:</label>
+        <select
+          id='Country'
+          value={search.country}
+          name="country"
+          onChange={handleChange}
+        >
+        <option value="">--- Seleccione un Pais ---</option>
+        {countries.map( country => (
+          <option
+            key={country.code}
+            value={country.code}
+            >{country.name}</option>
+        ))}
+        </select>
       </div>
+      <input className={styles.submit} type="submit" value="Consultar Clima" />
     </form>
   )
 }
