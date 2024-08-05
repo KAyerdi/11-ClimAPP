b6279a4a087ae7824106a4eb75400869f1730d04
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-08-05 17:59:34 -0300

    Actualizando proyecto

--- clima-react-typescript/src/hooks/useWeather.tsx
+++ clima-react-typescript/src/hooks/useWeather.tsx
@@ -1,5 +1,18 @@
 import axios from "axios"
-import { SearchType } from "../types"
+import { SearchType, Weather } from "../types"
+
+
+//TYPE GUARD o ASSERTIONS
+function isWeatherResponse(weather: unknown) : weather is Weather {
+  return(
+    Boolean(weather) &&
+    typeof weather === 'object' &&
+    typeof (weather as Weather).name === 'string' &&
+    typeof (weather as Weather).main.temp === 'number' &&
+    typeof (weather as Weather).main.temp_max === 'number' &&
+    typeof (weather as Weather).main.temp_min === 'number'
+  )
+}
 
 export default function useWeather() {
 
@@ -10,7 +23,36 @@ export default function useWeather() {
         const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
         const {data} = await axios(geoUrl)
-        console.log(data)
+        const lat = data[0].lat
+        const lon = data[0].lon
+
+        //console.log(lat)
+        //console.log(lon)
+
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+        //const {data: weatherResult} = await axios<Weather>(weatherUrl)
+        //console.log(weatherResult.main.temp)
+        //console.log(weatherResult.main.temp_min)
+        //console.log(weatherResult.main.temp_max)
+
+        //Castear el type
+        //const {data: weatherResult} = await axios(weatherUrl)
+        //console.log(weatherResult.temp)
+        //console.log(weatherResult.main.temp_min)
+        //console.log(weatherResult.main.temp_max)
+        //
+        //
+        //Type Guards
+        const {data: weatherResult} = await axios(weatherUrl)
+        const result = isWeatherResponse(weatherResult)
+        if(result){
+          console.log(weatherResult.name)
+        } else {
+          console.log('Respuesta mal formada')
+        }
+        
+
 
     } catch (error) {
       console.log(error)
