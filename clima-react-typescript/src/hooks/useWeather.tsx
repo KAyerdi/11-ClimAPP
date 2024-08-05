0a87da03f15283107744bc46aebe840a2a3f6914
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-08-05 11:48:43 -0300

    conexion a api con axios

--- clima-react-typescript/src/hooks/useWeather.tsx
+++ clima-react-typescript/src/hooks/useWeather.tsx
@@ -4,8 +4,14 @@ import { SearchType } from "../types"
 export default function useWeather() {
 
   const fetchWeather = async (search : SearchType) => {
+
+    const appId = 'b21346c52c0d259868ab4a4b0f923355'
     try {
-        const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`
+        const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+
+        const {data} = await axios(geoUrl)
+        console.log(data)
+
     } catch (error) {
       console.log(error)
     }
